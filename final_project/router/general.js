c19c059859eb373b3f7865d53bd9bed5d778d845
Tom Johnston <tom.johnston.2017@live.rhul.ac.uk>
2024-03-27 10:21:35 -0400

    added code to get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let response = [];
+    for (const book of Object.entries(books)) {
+        if (book[1].author == author) {
+            response.push(book[1]);
+        }
+    }
+    if (response.length === 0) {
+        return res.status(404).json({message: "Author not found."});
+    } else {
+        res.send(JSON.stringify(response, null, 4));
+    }
 });
 
 // Get all books based on title
